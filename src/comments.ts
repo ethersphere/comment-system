3b15f3e9d30d67eb8ae406c180b21b88dcc1c91d
Vladan Tomić <tomic.vladan.mail@gmail.com>
2023-11-07 10:10:04 +0100

    fix: optional stamp for reads

--- src/comments.ts
+++ src/comments.ts
@@ -8,7 +8,10 @@ import { isComment } from './asserts/models.assert'
 import { numberToFeedIndex } from './uitls/feeds'
 import { Options } from './model/options.model'
 
-async function prepareOptions(options: Options = {}): Promise<Required<Options>> {
+async function prepareOptions(
+  options: Options = {},
+  stampRequired = true,
+): Promise<Optional<Required<Options>, 'stamp'>> {
   const beeApiUrl = options.beeApiUrl ?? BEE_URL
   const beeDebugApiUrl = options.beeDebugApiUrl ?? BEE_DEBUG_URL
   let { identifier, stamp } = options
@@ -21,7 +24,7 @@ async function prepareOptions(options: Options = {}): Promise<Required<Options>>
     throw new Error('Cannot generate private key from an invalid URL')
   }
 
-  if (!stamp) {
+  if (!stamp && stampRequired) {
     const usableStamp = await getUsableStamp(beeDebugApiUrl)
 
     if (!usableStamp) {
@@ -39,8 +42,16 @@ async function prepareOptions(options: Options = {}): Promise<Required<Options>>
   }
 }
 
+function prepareWriteOptions(options: Options = {}): Promise<Required<Options>> {
+  return prepareOptions(options) as Promise<Required<Options>>
+}
+
+function prepareReadOptions(options: Options = {}): Promise<Omit<Required<Options>, 'stamp'>> {
+  return prepareOptions(options, false)
+}
+
 export async function writeComment(comment: CommentRequest, options?: Options) {
-  const { identifier, stamp, beeApiUrl } = await prepareOptions(options)
+  const { identifier, stamp, beeApiUrl } = await prepareWriteOptions(options)
 
   const privateKey = getPrivateKeyFromIdentifier(identifier)
 
@@ -56,7 +67,7 @@ export async function writeComment(comment: CommentRequest, options?: Options) {
 }
 
 export async function readComments(options?: Options): Promise<Comment[]> {
-  const { identifier, beeApiUrl } = await prepareOptions(options)
+  const { identifier, beeApiUrl } = await prepareReadOptions(options)
 
   const bee = new Bee(beeApiUrl)
 
